337fbe0460859fccbaa3a5ef4c4c837fa7ceceb1
Charl Smit <charlsmit96@gmail.com>
2022-11-10 21:58:51 +0200

    Add localsettings

--- tenantrace_api/settings.py
+++ tenantrace_api/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'tenantrace_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -117,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from localsettings import *
+except ImportError:
+    pass
